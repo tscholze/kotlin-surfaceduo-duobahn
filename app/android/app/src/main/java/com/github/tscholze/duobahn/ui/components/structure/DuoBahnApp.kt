74dd78a70f863b0e4dbd03489c2a0d7b72189a94
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2022-01-21 16:37:50 +0100

    [App] First steps in introducing the Surface Duo SDK
    re #9

---
+++ app/android/app/src/main/java/com/github/tscholze/duobahn/ui/components/structure/DuoBahnApp.kt
@@ -0,0 +1,101 @@
+package com.github.tscholze.duobahn.ui.components.structure
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneLayout
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneMode
+import com.microsoft.device.dualscreen.twopanelayout.navigateToPane1
+import com.microsoft.device.dualscreen.twopanelayout.navigateToPane2
+
+@ExperimentalAnimationApi
+@ExperimentalUnitApi
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun NavigationRailApp(windowState: WindowState) {
+    // Extract window state information
+    val isDualScreen = windowState.isDualScreen()
+    val isDualPortrait = windowState.isDualPortrait()
+    val isDualLandscape = windowState.isDualLandscape()
+    val foldSize = windowState.foldSize.dp
+
+    // Set up starting route for navigation in pane 1
+    var currentRoute by rememberSaveable { mutableStateOf(navDestinations[0].route) }
+    val updateRoute: (String) -> Unit = { newRoute -> currentRoute = newRoute }
+
+    // Set up variable to store selected image id
+    var imageId: Int? by rememberSaveable { mutableStateOf(null) }
+    val updateImageId: (Int?) -> Unit = { newId -> imageId = newId }
+
+    TwoPaneLayout(
+        paneMode = TwoPaneMode.HorizontalSingle,
+        pane1 = {
+            Pane1(isDualScreen, isDualPortrait, imageId, updateImageId, currentRoute, updateRoute)
+        },
+        pane2 = {
+            Pane2(isDualPortrait, isDualLandscape, foldSize, imageId, updateImageId, currentRoute)
+        },
+    )
+
+    // If only one pane is being displayed, make sure the correct pane is displayed
+    if (!isDualPortrait) {
+        if (imageId != null)
+            navigateToPane2()
+        else
+            navigateToPane1()
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun Pane1(
+    isDualScreen: Boolean,
+    isDualPortrait: Boolean,
+    imageId: Int?,
+    updateImageId: (Int?) -> Unit,
+    currentRoute: String,
+    updateRoute: (String) -> Unit
+) {
+    ShowWithNav(isDualScreen, isDualPortrait, imageId, updateImageId, currentRoute, updateRoute)
+}
+
+@ExperimentalUnitApi
+@ExperimentalMaterialApi
+@Composable
+fun Pane2(
+    isDualPortrait: Boolean,
+    isDualLandscape: Boolean,
+    foldSize: Dp,
+    imageId: Int?,
+    updateImageId: (Int?) -> Unit,
+    currentRoute: String,
+) {
+    // Retrieve selected image information
+    val selectedImage = imageId?.let { DataProvider.getImage(imageId) }
+
+    // Set up back press action to return to pane 1 and clear image selection
+    val onBackPressed = {
+        navigateToPane1()
+        updateImageId(null)
+    }
+    BackHandler { if (!isDualPortrait) onBackPressed() }
+
+    ItemDetailView(isDualPortrait, isDualLandscape, foldSize, selectedImage, currentRoute)
+    // If only one pane is being displayed, show a "back" icon
+    if (!isDualPortrait) {
+        ItemTopBar(
+            onClick = { onBackPressed() }
+        )
+    }
+}
