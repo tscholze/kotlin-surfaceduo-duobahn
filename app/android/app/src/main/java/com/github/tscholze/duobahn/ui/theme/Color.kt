38de951db0b3e7abe4fba58b1ca3bf9f44e4fc31
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2022-02-20 13:51:18 +0100

    Updated Theme.kt with actual color values
    re #16

--- app/android/app/src/main/java/com/github/tscholze/duobahn/ui/theme/Color.kt
+++ app/android/app/src/main/java/com/github/tscholze/duobahn/ui/theme/Color.kt
@@ -2,9 +2,40 @@ package com.github.tscholze.duobahn.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
+/**
+ * "Brand" color of an Autobahn which is used
+ * for signs, and icons along the road.
+ * */
 val AutobahnBlue = Color(color = 0xFF154889)
+
+/**
+ * Generated and stripped down values from:
+ * https://material-foundation.github.io/material-theme-builder
+ */
+
+val seed = Color(0xFF154889)
+val error = Color(0xFFba1b1b)
+
+//val md_theme_light_primary = Color(0xFF2a5ea7)
+val md_theme_light_primary = AutobahnBlue
+val md_theme_light_onPrimary = Color(0xFFffffff)
+val md_theme_light_secondary = Color(0xFF565f71)
+val md_theme_light_onSecondary = Color(0xFFffffff)
+val md_theme_light_error = Color(0xFFba1b1b)
+val md_theme_light_onError = Color(0xFFffffff)
+val md_theme_light_background = Color(0xFFfdfbff)
+val md_theme_light_onBackground = Color(0xFF1a1b1f)
+val md_theme_light_surface = Color(0xFFfdfbff)
+val md_theme_light_onSurface = Color(0xFF1a1b1f)
+
+//val md_theme_dark_primary = Color(0xFFa8c7ff)
+val md_theme_dark_primary = AutobahnBlue
+val md_theme_dark_onPrimary = Color(0xFF002f66)
+val md_theme_dark_secondary = Color(0xFFbec7dd)
+val md_theme_dark_onSecondary = Color(0xFF283141)
+val md_theme_dark_error = Color(0xFFffb4a9)
+val md_theme_dark_onError = Color(0xFF680003)
+val md_theme_dark_background = Color(0xFF1a1b1f)
+val md_theme_dark_onBackground = Color(0xFFe3e2e6)
+val md_theme_dark_surface = Color(0xFF1a1b1f)
+val md_theme_dark_onSurface = Color(0xFFe3e2e6)
--- app/android/app/src/main/java/com/github/tscholze/duobahn/ui/theme/Theme.kt
+++ app/android/app/src/main/java/com/github/tscholze/duobahn/ui/theme/Theme.kt
@@ -6,41 +6,49 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+// MARK: - Private properties -
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
 )
+
+private val DarkColorPalette = darkColors(
+
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    error = md_theme_dark_error,
+    onError = md_theme_dark_onError,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+)
+
+// MARK: - Composable elements -
 
 /**
  * Gets the app's theme.
  * It supports light and dark theme.
  */
 @Composable
-fun DuoBahnTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun DuoBahnTheme(
+    content: @Composable() () -> Unit
+) {
 
+    // Create theme based on input.
     MaterialTheme(
-        colors = colors,
+        colors = if (isSystemInDarkTheme()) DarkColorPalette else LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
