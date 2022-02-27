d919effef2216635f637dc46f0df59d4bd753d7d
MarcelReiter <mreiter@anfe.ma>
2022-02-27 23:41:15 +0100

    Enhancement: coil now loads webcam-image

--- app/android/app/src/main/java/com/github/tscholze/duobahn/ui/components/map/WebcamPreview.kt
+++ app/android/app/src/main/java/com/github/tscholze/duobahn/ui/components/map/WebcamPreview.kt
@@ -1,40 +1,36 @@
 package com.github.tscholze.duobahn.ui.components.map
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.github.tscholze.duobahn.R
 import com.github.tscholze.duobahn.data.domain.models.Webcam
-    
+
 @Composable
 fun WebcamPreview(
     modifier: Modifier = Modifier,
     webcam: Webcam,
 ) {
     Surface(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
+        modifier = modifier,
         elevation = 4.dp
     ) {
-        Box(contentAlignment = Alignment.Center){
-            // TODO use real image
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(2f)
-                    .background(Color.Gray)
+        Box(contentAlignment = Alignment.Center) {
+            Image(
+                painter = rememberImagePainter(webcam.thumbnailUrlString),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth().wrapContentHeight()
             )
 
             Image(
