e9e21917860499b419cbb92627715955cd2b6889
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2022-05-26 22:30:22 +0200

    Added user location permission requesting
    re #23

--- app/android/app/src/main/java/com/github/tscholze/duobahn/ui/components/map/BingMapView.kt
+++ app/android/app/src/main/java/com/github/tscholze/duobahn/ui/components/map/BingMapView.kt
@@ -1,14 +1,27 @@
 package com.github.tscholze.duobahn.ui.components.map
 
 import android.graphics.BitmapFactory
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.github.tscholze.duobahn.BuildConfig.CREDENTIALS_KEY
+import com.github.tscholze.duobahn.R
 import com.github.tscholze.duobahn.data.network.repositories.UnprocessedDataRepository
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
 import com.microsoft.maps.*
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.get
@@ -16,7 +29,11 @@ import org.koin.androidx.compose.get
 
 // MARK: - Internal composable views -
 
+/**
+ * Compose wrapper for a Microsoft Bing Map View.
+ */
 @Composable
+@OptIn(ExperimentalPermissionsApi::class)
 fun BingMap(repository: UnprocessedDataRepository = get()) {
 
     // Get map
@@ -35,8 +52,17 @@ fun BingMap(repository: UnprocessedDataRepository = get()) {
             tag = it.id
             title = it.title
             location = it.location
-            image =
-                MapImage(BitmapFactory.decodeResource(LocalContext.current.resources, it.imageId))
+            image = MapImage(
+                BitmapFactory.decodeResource(
+                    LocalContext.current.resources,
+                    it.imageId
+                )
+            )
+            flyout = MapFlyout().apply {
+                title = it.title
+                description =
+                    "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+            }
         }
 
         mapLayer.elements.add(pin)
@@ -44,6 +70,46 @@ fun BingMap(repository: UnprocessedDataRepository = get()) {
 
     // Set map to container
     BingMapContainer(map = map)
+
+
+    // Check for permissions
+    // Location permission
+    val permission = rememberPermissionState(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    when (permission.status) {
+        // If the camera permission is granted, then show screen with the feature enabled
+        PermissionStatus.Granted -> {
+            map.userLocation.startTracking(
+                GPSMapLocationProvider.Builder(LocalContext.current).build()
+            )
+            map.userLocation.trackingMode = MapUserLocationTrackingMode.CENTERED_ON_USER
+        }
+        is PermissionStatus.Denied -> {
+            val openDialog = remember { mutableStateOf(true) }
+            if (openDialog.value) {
+                AlertDialog(
+                    onDismissRequest = { openDialog.value = false },
+                    title = { Text(text = stringResource(R.string.alert_location_request_title)) },
+                    text = { Text(stringResource(R.string.alert_location_request_message)) },
+                    buttons = {
+                        Row(
+                            modifier = Modifier.padding(all = 8.dp),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            // Cancel button
+                            TextButton(onClick = { openDialog.value = false }) {
+                                Text(stringResource(R.string.misc_no))
+                            }
+
+                            // Ok button
+                            TextButton(onClick = { permission.launchPermissionRequest() }) {
+                                Text(stringResource(R.string.misc_yes))
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
 }
 
 // MARK: - Private composable views -
@@ -51,13 +117,13 @@ fun BingMap(repository: UnprocessedDataRepository = get()) {
 @Composable()
 private fun BingMapContainer(map: MapView) {
     LaunchedEffect(map) {
-        // map.setScene(MapScene.createFromLocation(point), MapAnimationKind.NONE)
+        //
     }
 
     val coroutineScope = rememberCoroutineScope()
     AndroidView(factory = { map }) {
         coroutineScope.launch {
-            // it.centerTo(lat = lat, lng = lng)
+            //
         }
     }
 }
@@ -102,5 +168,3 @@ fun MapView.centerTo(lat: Double, lng: Double) {
     val point = Geopoint(lat, lng)
     setScene(MapScene.createFromLocation(point), MapAnimationKind.NONE)
 }
-
-// https://github.com/microsoft/surface-duo-dual-screen-experience-example/blob/main/app/src/emulator/java/com/microsoft/device/samples/dualscreenexperience/presentation/store/map/BingMapController.kt
